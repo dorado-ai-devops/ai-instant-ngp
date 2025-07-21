f65c5600acfbad39f3fa81a603b1c42adc4c09ff
root <root@deletepc.localdomain>
2025-07-21 21:50:37 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-DATA_PATH=${DATA_PATH:-/data/lego-ds}  # Valor por defecto si no se define la env var
+DATA_PATH=${DATA_PATH:-/data/lego-ds}  
 /app/instant-ngp/build/instant-ngp --mode nerf --scene "$DATA_PATH" --no-gui
